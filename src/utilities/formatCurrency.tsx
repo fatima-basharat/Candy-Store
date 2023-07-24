295e9804a99e412368004c7c6cd501d73e78e6c6
Fatima Basharat <fatima8746@gmail.com>
2023-07-24 16:45:42 -0400

    initial commit

---
+++ src/utilities/formatCurrency.tsx
@@ -0,0 +1,5 @@
+const CURRENCT_FORMATTER = new Intl.NumberFormat(undefined, {currency : "USD", style: "currency"})
+
+export function formatCurrency(number : number){
+    return CURRENCT_FORMATTER.format(number)
+}
